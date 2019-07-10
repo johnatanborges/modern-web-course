b59402be3c4776d553941afd3fd497f841cf3b62
Johnatan L Borges <johnatanborges@hotmail.com>
2019-07-10 17:09:34 -0300

    Higher-Order Function

--- Function.js/first-class-Object.js
+++ Function.js/first-class-Object.js
@@ -1,5 +1,5 @@
 // Function in JS is First-Class Object (Citizens)
-// Higher-order function
+// Higher-order Function
 
 // literal function
 function fun1() {};
@@ -9,3 +9,27 @@ const fun2 = function() {};
 
 const array = [function (a, b) { return a + b }, fun1, fun2];
 console.log(array[0](2, 3)); // 5
+
+// function in objects
+const obj = {};
+obj.falar = function () { return 'Opa'};
+console.log(obj.falar()); // Opa
+
+// function param
+function run(fun) {
+    fun()
+}
+
+run(function() { console.log('Corra...')}) // Corra...
+
+// function return
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c);
+    }
+}
+
+soma(2, 3)(4); // 9
+
+const cincoMais = soma(2, 3);
+cincoMais(4); // 9
