cb17d6b959a70f1383e2f6d8bf2b0b5340225caf
Johnatan L Borges <johnatanborges@hotmail.com>
2019-07-16 00:19:26 -0300

    Exercise constructor

---
+++ function-js/exercise-constructor-function.js
@@ -0,0 +1,23 @@
+// class Pessoa {
+//     constructor(nome) {
+//         this.nome = nome;
+//     }
+
+//     falar() {
+//         console.log(`Meu nome é ${this.nome}`);
+//     }
+// }
+
+// const p1 = new Pessoa('João');
+// p1.falar(); // Meu nome é João
+
+function Pessoa (nome) {
+    this.nome = nome;
+
+    this.falar = function() {
+        console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p1 = new Pessoa('João');
+p1.falar(); // Meu nome é João
