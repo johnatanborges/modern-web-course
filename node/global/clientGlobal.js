ee177cdab813de58237b55597e78ea708e2fbb06
Johnatan L Borges <johnatanborges@hotmail.com>
2020-01-08 23:03:57 -0300

    Global scope

---
+++ node/global/clientGlobal.js
@@ -0,0 +1,6 @@
+require('./objectGlobal')
+
+console.log(myApp.hello()) // I am in everywhere!
+
+myApp.name = 'Eita' // Eita
+console.log(myApp.name)
---
+++ node/global/objectGlobal.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+global.myApp = {
+    hello() {
+        return 'I am in everywhere!'
+    },
+    name: 'My System'
+}
