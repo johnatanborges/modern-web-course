1b7e4f2f1a6b609fc2b238c6a08e58d2cb936c05
Johnatan <johnatan@spacecom.com.br>
2021-07-06 21:14:32 -0300

    Translate

--- fundamentals-js/function-01.js
+++ fundamentals-js/function-01.js
@@ -1,18 +1,18 @@
-// Função sem retorno
-function imprimirSoma(a, b) {
+// Function whitout return
+function printSum(a, b) {
     console.log(a + b);
 }
 
-imprimirSoma(2, 3); // 5
-imprimirSoma(2); // NaN
-imprimirSoma(2, 10, 4, 5, 6, 7); // 12
-imprimirSoma(); // NaN
+printSum(2, 3); // 5
+printSum(2); // NaN
+printSum(2, 10, 4, 5, 6, 7); // 12
+printSum(); // NaN
 
-// Função com retorno
-function soma(a, b = 1) {
+// Function with return
+function sum(a, b = 1) {
     return a + b;
 }
 
-console.log(soma(2, 3)); // 5
-console.log(soma(2)); // 3
-console.log(soma()); // NaN
+console.log(sum(2, 3)); // 5
+console.log(sum(2)); // 3
+console.log(sum()); // NaN
