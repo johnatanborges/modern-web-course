6c5422b8c42256dc7adca361d486f321f5aa302f
Johnatan <johnatan@spacecom.com.br>
2021-07-09 20:47:02 -0300

    Translate

--- fundamentals-js/arithmetic-operator.js
+++ fundamentals-js/arithmetic-operator.js
@@ -1,9 +1,9 @@
 const [a, b, c, d] = [3, 5, 1, 15];
 
-const soma = a + b + c + d;
-const subtracao = d - b;
-const multiplicacao = a * b;
-const divisao = d / a;
-const modulo = a % 2;
+const sum = a + b + c + d;
+const sub = d - b;
+const multiplication = a * b;
+const division = d / a;
+const remainder = a % 2;
 
-console.log(soma, subtracao, multiplicacao, divisao, modulo); // 24 10 15 5 1
+console.log(sum, sub, multiplication, division, remainder); // 24 10 15 5 1
