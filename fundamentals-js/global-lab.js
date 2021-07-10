41fbb23e36fd7092214c3be527b5f0c5b5a77dca
Johnatan <johnatan@spacecom.com.br>
2021-07-09 21:06:43 -0300

    Translate

--- fundamentals-js/global-lab.js
+++ fundamentals-js/global-lab.js
@@ -4,18 +4,18 @@ globalThis.b = 123;
 
 this.c = 456;
 this.d = false;
-this.e = 'teste';
+this.e = 'test';
 
 console.log(a); // 3
 console.log(global.b); // 123
 console.log(this.c); // 456
 console.log(module.exports.c); // 456
 console.log(module.exports === this); // true
-console.log(module.exports); // { c: 456, d: false, e: 'teste' }
+console.log(module.exports); // { c: 456, d: false, e: 'test' }
 
 // no let and var variable
 abc = 3;
 console.log(global.abc); // 3
 
-// module.exports = { e: 456, f: false, g: 'teste" }
-console.log(module.exports); // { c: 456, d: false, e: 'teste' }
+// module.exports = { e: 456, f: false, g: 'test" }
+console.log(module.exports); // { c: 456, d: false, e: 'test' }
