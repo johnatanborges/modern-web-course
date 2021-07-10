6f1b5a896863cadd6b291afcecd49cadda22ca30
Johnatan <johnatan@spacecom.com.br>
2021-07-09 21:31:07 -0300

    Translate

--- fundamentals-js/string.js
+++ fundamentals-js/string.js
@@ -1,18 +1,18 @@
-const escola = 'Cod3r';
+const school = 'Cod3r';
 
-console.log(escola.charAt(4)); // r
-console.log(escola.charAt(5)); // (vazio)
+console.log(school.charAt(4)); // r
+console.log(school.charAt(5)); // (empty)
 
-console.log(escola.charCodeAt(3)); // Retorna o código Unicode do parãmetro -> output: 51
+console.log(school.charCodeAt(3)); // Return unicode -> output: 51
 
-console.log(escola.indexOf('3')); // Retorna o indíce do elemento analisado, caso exista na variável -> output: 3
+console.log(school.indexOf('3')); // output: 3
 
-console.log(escola.substring(1)); // Retorna uma string do índice 1 para frente -> output: od3r
+console.log(school.substring(1)); // output: od3r
 
-console.log(escola.substring(0,3)); // Retorna uma string com 3 caracteres a partir do índice 0 -> output: Cod
+console.log(school.substring(0,3)); //output: Cod
 
-console.log('Escola '.concat(escola).concat('!')); // Escola Cod3r!
+console.log('school '.concat(school).concat('!')); // school Cod3r!
 
-console.log(escola.replace(3, 'e')); // Coder
+console.log(school.replace(3, 'e')); // Coder
 
 console.log('Ana,Maria,Pedro'.split(',')); // ['Ana', 'Maria', 'Pedro']
