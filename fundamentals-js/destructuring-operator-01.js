edff88b6eeb8758a3a1e95d0f7e9be78a5754ec4
Johnatan <johnatan@spacecom.com.br>
2021-07-17 00:04:12 -0300

    Translate

--- fundamentals-js/destructuring-operator-01.js
+++ fundamentals-js/destructuring-operator-01.js
@@ -15,8 +15,8 @@ console.log(name, age); // Ana 15
 const { name: n, age: a} = person;
 console.log(n, a); // Ana 15
 
-const { lastname, happy = true } = person;
-console.log(lastname, happy); // undefined true
+const { lastName, happy = true } = person;
+console.log(lastMame, happy); // undefined true
 
 const { addres: { street, number, cep } } = person;
 console.log(street, number, cep); // ABCD 100 undefined
