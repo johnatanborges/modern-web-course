5653fda4b530c5f83b18a61a4d95c9e29d65837e
Johnatan <j.lohanborges@gmail.com>
2021-07-17 18:46:15 -0300

    Translate

--- array-js/simulating-array.js
+++ array-js/simulating-array.js
@@ -1,12 +1,12 @@
-const quaseArray = { 0: 'Rafael', 1: 'Ana', 2: 'Bia' }
-console.log(quaseArray) // { '0': 'Rafael', '1': 'Ana', '2': 'Bia' }
+const almostArray = { 0: 'Rafael', 1: 'Ana', 2: 'Bia' }
+console.log(almostArray) // { '0': 'Rafael', '1': 'Ana', '2': 'Bia' }
 
-Object.defineProperty(quaseArray, 'toString', {
+Object.defineProperty(almostArray, 'toString', {
     value: function() { return Object.values(this) },
     enumerable: false
 })
 
-console.log(quaseArray[0]) // Rafael
+console.log(almostArray[0]) // Rafael
 
-const meuArray = [ 'Rafael', 'Ana', 'Bia' ]
-console.log(quaseArray.toString(), meuArray) // [ 'Rafael', 'Ana', 'Bia' ] [ 'Rafael', 'Ana', 'Bia' ]
+const myArray = [ 'Rafael', 'Ana', 'Bia' ]
+console.log(almostArray.toString(), myArray) // [ 'Rafael', 'Ana', 'Bia' ] [ 'Rafael', 'Ana', 'Bia' ]
