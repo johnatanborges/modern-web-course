645d5e91459cba4367d6bd96690394442e13ebd2
Johnatan <j.lohanborges@gmail.com>
2021-07-25 23:21:00 -0300

    Translate

--- control-structures-js/conditional-IF-ELSE-IF.js
+++ control-structures-js/conditional-IF-ELSE-IF.js
@@ -1,24 +1,24 @@
-Number.prototype.entre = function(inicio, fim) {
-    return this >= inicio && this <= fim;
+Number.prototype.between = function(init, end) {
+    return this >= init && this <= end;
 };
 
-const imprimirResultado = function(nota) {
-    if(nota.entre(9, 10)) {
-        console.log('Quadro de Honra');
-    } else if(nota.entre(7, 8.99)) {
-        console.log('Aprovado');
-    } else if(nota.entre(4, 6.99)) {
-        console.log('Recuperação');
-    } else if(nota.entre(0, 3.99)) {
-        console.log('Reprovado');
+const printResult = function(grade) {
+    if(grade.between(9, 10)) {
+        console.log('Honor board');
+    } else if(grade.between(7, 8.99)) {
+        console.log('Approved');
+    } else if(grade.between(4, 6.99)) {
+        console.log('Recovery');
+    } else if(grade.between(0, 3.99)) {
+        console.log('Disapproved');
     } else {
-        console.log('Nota inválida');
+        console.log('Invalid grade');
     }
 };
 
-imprimirResultado(10); // Quadro de Honra
-imprimirResultado(8.9); // Aprovado
-imprimirResultado(6.55); // Recuperação
-imprimirResultado(2); // Reprovado
-imprimirResultado(-1); // Nota inválida
-imprimirResultado(11); // Nota inválida
+printResult(10); // Honor board
+printResult(8.9); // Approved
+printResult(6.55); // Recovery
+printResult(2); // Disapproved
+printResult(-1); // Invalid grade
+printResult(11); // Invalid grade
